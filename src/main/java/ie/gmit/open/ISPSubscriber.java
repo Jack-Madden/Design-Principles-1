8f26e3c2ff0c47b4f41656e95d610d63da72e12f
Jack Madden <g00338579@gmit.ie>
2020-03-27 23:34:40 +0000

    Demonstration of single responsibility and open-closed principles

---
+++ src/main/java/ie/gmit/open/ISPSubscriber.java
@@ -0,0 +1,34 @@
+package ie.gmit.open;
+
+import java.util.List;
+
+public class ISPSubscriber extends Subscriber {
+
+    private long freeUsage;
+
+    public ISPSubscriber(Long subscriberId, String address, Long phoneNumber, int baseRate, long freeUsage) {
+        super(subscriberId, address, phoneNumber, baseRate);
+        this.freeUsage = freeUsage;
+    }
+
+    public double calculateBill() {
+        List<InternetSessionHistory.InternetSession> sessions = InternetSessionHistory.getCurrentSessions(getSubscriberId());
+        long totalData = sessions.stream().mapToLong(InternetSessionHistory.InternetSession::getDataUsed).sum();
+        long chargeableData = totalData - freeUsage;
+        return chargeableData*getBaseRate()/100.0;
+    }
+
+    /**
+     * @return the freeUsage
+     */
+    public long getFreeUsage() {
+        return freeUsage;
+    }
+
+    /**
+     * @param freeUsage the freeUsage to set
+     */
+    public void setFreeUsage(long freeUsage) {
+        this.freeUsage = freeUsage;
+    }
+}
